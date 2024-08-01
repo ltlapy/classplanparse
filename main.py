87e771be61fb16ccd4558e57c94a67572d84965c
ltlapy <lapy@lapy.link>
2024-08-01 13:46:31 +0900

    Linting, export to valid csv

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from bigxml import Parser, xml_handle_element, XMLElement
 from dataclasses import dataclass
-from typing import Dict, List, TypedDict, Optional
+from typing import Dict, TypedDict, Optional
 
 WORKSPACE = "tmp"
+
 
 # region typedef_header
 
@@ -50,14 +51,14 @@ def handle_table(node: XMLElement):
         text: str = ""
         colspan: int = 1
         rowspan: int = 1
-        charPrID: int = 1 
-        
+        charPrID: int = 1
+
         # colspan, rowspan
         @xml_handle_element("cellSpan")
         def handle_cellSpan(self, node: XMLElement):
             self.colspan = int(node.attributes["colSpan"])
             self.rowspan = int(node.attributes["rowSpan"])
-            
+
         # text, charPrID
         @xml_handle_element("subList", "p", "run")  # 내부의 첫 번째 문장만 가져온다. p - run - t 구조가 항상 유지됨을 가정한다.
         def handle_context(self, node: XMLElement):
@@ -68,10 +69,10 @@ def handle_table(node: XMLElement):
     for row in node.iter_from("tr"):
         cols = [col for col in row.iter_from(tc)]
         yield cols
-        
+
 
 # endregion typedef_section0
-    
+
 cat1_charprid = None
 cat2_charprid = None
 tblhader_charprid = None
@@ -89,48 +90,52 @@ with open(f"{WORKSPACE}/Contents/header.xml", "rb") as stream:
             tblhader_charprid = charpr.id
 
 with open(f"{WORKSPACE}/Contents/section0.xml", "rb") as stream:
-    print("대분류,소분류,이수구분,학년,과목번호,분반,과목명,학점,시수,시간,강의실,교수명,수업정보,교수법,비고")
-    
-    cat1 = ""
-    cat2 = ""
-    
-    iterlimit = 1000000
-    rowSpanCache: Dict[int, list] = {}  # {writeColIdx: [remaining, text]} (remaining > 0)
-    for row in Parser(stream).iter_from(handle_table):
-        catPrinted = False  # for prepend cat1 and cat2
-        writeColIdx = 0
-        for i, cell in enumerate(row):
-            if cell.charPrID == cat1_charprid:
-                cat1 = cell.text
-                break
-            elif cell.charPrID == cat2_charprid:
-                cat2 = cell.text
-                break
-            elif cell.charPrID == tblhader_charprid:
-                # Do nothing
-                break
-            else:
-                if not catPrinted:
-                    print(f"{cat1},{cat2},", end="")
-                    catPrinted = True
-                    writeColIdx += 2
-                    
-                while rowSpanCache.get(writeColIdx):
-                    print(rowSpanCache[writeColIdx][1], end=",")
-                    rowSpanCache[writeColIdx][0] -= 1
-                    if rowSpanCache[writeColIdx][0] < 1:
-                        rowSpanCache.pop(writeColIdx)
+    with open('out.csv', 'w', encoding='utf8') as outfile:
+        outfile.write("대분류,소분류,이수구분,학년,과목번호,분반,과목명,학점,시수,시간,강의실,교수명,수업정보,교수법,비고\n")
+
+        cat1 = ""
+        cat2 = ""
+
+        rowSpanCache: Dict[int, list] = {}  # {writeColIdx: [remaining, text]} (remaining > 0)
+        for row in Parser(stream).iter_from(handle_table):
+            catPrinted = False  # for prepend cat1 and cat2
+            writeColIdx = 0
+            for i, cell in enumerate(row):
+                if cell.charPrID == cat1_charprid:
+                    cat1 = cell.text
+                    break
+                elif cell.charPrID == cat2_charprid:
+                    cat2 = cell.text
+                    break
+                elif cell.charPrID == tblhader_charprid:
+                    # Do nothing
+                    break
+                else:
+                    if not catPrinted:
+                        outfile.write(f"{cat1},{cat2},")
+                        catPrinted = True
+                        writeColIdx += 2
+
+                    while rowSpanCache.get(writeColIdx):
+                        txt: str = rowSpanCache[writeColIdx][1]
+                        if ',' in txt:
+                            txt = f'"txt"'
+                        outfile.write(f'"{txt}",')
+                        rowSpanCache[writeColIdx][0] -= 1
+                        if rowSpanCache[writeColIdx][0] < 1:
+                            rowSpanCache.pop(writeColIdx)
+                        writeColIdx += 1
+
+                    if cell.rowspan > 1:
+                        rowSpanCache[writeColIdx] = [cell.rowspan - 1, cell.text]
+
+                    if ',' in cell.text:
+                        outfile.write(f'"{cell.text}",')
+                    else:
+                        outfile.write(f'{cell.text},')
                     writeColIdx += 1
-                
-                if cell.rowspan > 1:
-                    rowSpanCache[writeColIdx] = [cell.rowspan - 1, cell.text]
-                    
-                print(f"{cell.text}", end=",")
-                writeColIdx += 1
-        
-        if catPrinted:
-                print()
-        
-        iterlimit -= 1
-        if iterlimit < 1:
-            break
+
+            if catPrinted:
+                outfile.write('\n')
+
+print('Done!')
